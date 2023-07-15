638f49b5c9ca93a16b43818fd8f9782f797cfd47
anilvashisth <80627006+anilvashisth@users.noreply.github.com>
2023-07-15 16:27:04 +0530

    First Commit Program 7

---
+++ Chapter01/Program007.cpp
@@ -0,0 +1,14 @@
+//Lecture 7 Area of a Square
+#include<iostream>
+using namespace std;
+
+int main(){
+    int side,AreaofSquare;
+
+    cout<<"Enter Side of a Square : " ;
+    cin>> side;
+    AreaofSquare=side*side; 
+    cout<< "Area of Square is :"<<AreaofSquare ;
+    
+    return 0;
+}
