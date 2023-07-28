68f23a4f35159a342801692d9d8d1211c549ff18
anilvashisth <80627006+anilvashisth@users.noreply.github.com>
2023-07-28 20:49:47 +0530

    First Commit Program 9

---
+++ Chapter01/Program009.cpp
@@ -0,0 +1,18 @@
+/*Lecture 9
+#include<iostream>
+using namespace std;
+
+int main(){
+    float radius,volumnofsphere,pi=3.14,lsa,tsa;
+    cout<<"The radius of sphere:";
+    cin>>radius;
+    volumnofsphere=(4*pi*radius*radius*radius)/3;
+    lsa=4*pi*radius*radius;
+    tsa=4*pi*radius*radius;
+
+    cout<<"The Volumn of Sphere : "<<volumnofsphere<<endl;
+    cout<<"The LSA of Sphere : "<< lsa<<endl;
+    cout<<"The TSA of Sphere : "<<tsa <<endl;
+
+      return 0;
+}
